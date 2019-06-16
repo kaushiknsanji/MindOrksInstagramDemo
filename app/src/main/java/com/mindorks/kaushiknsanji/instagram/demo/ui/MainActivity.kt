970732a68ddeab2476cd04913c7dbd4deae67c12
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2019-06-16 15:58:22 +0530

    Initial Commit with Project structure

---
+++ app/src/main/java/com/mindorks/kaushiknsanji/instagram/demo/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mindorks.kaushiknsanji.instagram.demo.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mindorks.kaushiknsanji.instagram.demo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
