042270e7d2427dc19c6aab6ca5d84656f3c47441
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2019-07-23 13:15:12 +0530

    docs: Rewrite comments in Resource

--- app/src/main/java/com/mindorks/kaushiknsanji/instagram/demo/utils/common/Resource.kt
+++ app/src/main/java/com/mindorks/kaushiknsanji/instagram/demo/utils/common/Resource.kt
@@ -17,28 +17,28 @@ data class Resource<out T> private constructor(val status: Status, val dataEvent
         /**
          * Factory method for [Resource] with status [Status.SUCCESS].
          *
-         * @param data The content to be wrapped in the [Resource]
+         * @param data The content to be wrapped in the [dataEvent] of the [Resource]
          */
         fun <T> success(data: T? = null): Resource<T> = Resource(Status.SUCCESS, data?.let { Event(it) })
 
         /**
          * Factory method for [Resource] with status [Status.ERROR].
          *
-         * @param data The content to be wrapped in the [Resource]
+         * @param data The content to be wrapped in the [dataEvent] of the [Resource]
          */
         fun <T> error(data: T? = null): Resource<T> = Resource(Status.ERROR, data?.let { Event(it) })
 
         /**
          * Factory method for [Resource] with status [Status.LOADING].
          *
-         * @param data The content to be wrapped in the [Resource]
+         * @param data The content to be wrapped in the [dataEvent] of the [Resource]
          */
         fun <T> loading(data: T? = null): Resource<T> = Resource(Status.LOADING, data?.let { Event(it) })
 
         /**
          * Factory method for [Resource] with status [Status.UNKNOWN].
          *
-         * @param data The content to be wrapped in the [Resource]
+         * @param data The content to be wrapped in the [dataEvent] of the [Resource]
          */
         fun <T> unknown(data: T? = null): Resource<T> = Resource(Status.UNKNOWN, data?.let { Event(it) })
     }
