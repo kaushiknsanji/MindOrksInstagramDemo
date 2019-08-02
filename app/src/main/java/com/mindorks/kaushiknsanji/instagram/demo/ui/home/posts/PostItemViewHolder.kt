afb6ef6572455af63852538841262b48c52c2578
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2019-08-02 16:26:25 +0530

    feat: Add Double Tap to Like on Post Item

--- app/src/main/java/com/mindorks/kaushiknsanji/instagram/demo/ui/home/posts/PostItemViewHolder.kt
+++ app/src/main/java/com/mindorks/kaushiknsanji/instagram/demo/ui/home/posts/PostItemViewHolder.kt
@@ -1,8 +1,11 @@
 package com.mindorks.kaushiknsanji.instagram.demo.ui.home.posts
 
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.view.GestureDetectorCompat
 import androidx.lifecycle.Observer
 import com.bumptech.glide.request.RequestOptions
 import com.mindorks.kaushiknsanji.instagram.demo.R
@@ -36,8 +39,72 @@ class PostItemViewHolder(container: ViewGroup) :
      * Initializes the Layout of the [itemView].
      */
     override fun setupView(itemView: View) {
-        // Register a click listener on the "Heart" ImageButton, to enable like/unlike Post
+        // Register a Click Listener on the "Heart" ImageButton, to enable like/unlike Post
         itemView.imgbtn_home_item_toggle_post_like.setOnClickListener { itemViewModel.onLikeClick() }
+
+        // Create a Double Tap Gesture Detection on Post Photo, to enable like/unlike Post on Double Tap action
+        val postPhotoDoubleTapGestureDetector = GestureDetectorCompat(
+            itemView.context,
+            object : GestureDetector.SimpleOnGestureListener() {
+
+                /**
+                 * Notified when a tap occurs with the down [MotionEvent]
+                 * that triggered it. This will be triggered immediately for
+                 * every down event. All other events should be preceded by this.
+                 *
+                 * @param event The down motion event.
+                 */
+                override fun onDown(event: MotionEvent?): Boolean {
+                    // Report as Consumed to receive future event for Double-Tap
+                    return true
+                }
+
+                /**
+                 * Notified when a single-tap occurs.
+                 *
+                 * Unlike [android.view.GestureDetector.OnGestureListener.onSingleTapUp], this
+                 * will only be called after the detector is confident that the user's
+                 * first tap is not followed by a second tap leading to a double-tap
+                 * gesture.
+                 *
+                 * @param event The down motion event of the single-tap.
+                 * @return true if the event is consumed, else false
+                 */
+                override fun onSingleTapConfirmed(event: MotionEvent?): Boolean {
+                    // Delegate the event to the system to handle
+                    itemView.performClick()
+                    // Report as Unconsumed
+                    return false
+                }
+
+                /**
+                 * Notified when a double-tap occurs.
+                 *
+                 * @param event The down motion event of the first tap of the double-tap.
+                 * @return true if the event is consumed, else false
+                 */
+                override fun onDoubleTap(event: MotionEvent?): Boolean {
+                    // Delegate to the ItemView's ViewModel to handle
+                    itemViewModel.onLikeClick()
+                    // Report as Consumed
+                    return true
+                }
+
+            }
+        )
+
+        // Register a Double Tap Listener using the Gesture Detection on Post Photo
+        itemView.image_home_item_post_photo.setOnTouchListener { _, event: MotionEvent ->
+            // Pass all the events to the Double Tap Gesture Detector and return its result
+            postPhotoDoubleTapGestureDetector.onTouchEvent(event)
+        }
+
+        // Register a Click Listener on the TextView that displays the number of Likes on the Post
+        itemView.text_home_item_post_like_count.setOnClickListener { itemViewModel.onLikeCountClick() }
+
+        // Register a Click Listener on the Post Item
+        itemView.setOnClickListener { itemViewModel.onItemClick() }
+
     }
 
     /**
