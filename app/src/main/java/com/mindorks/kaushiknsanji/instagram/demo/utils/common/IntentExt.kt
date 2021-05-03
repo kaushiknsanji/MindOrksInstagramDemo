48b54cfc98f2273b6ad5367e45fc4bd513714a47
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2021-05-03 23:09:28 +0530

    refactor: Remove unnecessary generics used in Intent extension

--- app/src/main/java/com/mindorks/kaushiknsanji/instagram/demo/utils/common/IntentExt.kt
+++ app/src/main/java/com/mindorks/kaushiknsanji/instagram/demo/utils/common/IntentExt.kt
@@ -12,12 +12,11 @@ import java.io.Serializable
 /**
  * Extension function on [Intent] to load the Intent Extras from [map].
  *
- * @param V the type of Intent Extra values which needs to [Serializable].
  * @param map The source [Map] containing the Intent Extras to load from.
  * @return Returns the same [Intent] object, for chaining multiple calls
  * into a single statement.
  */
-fun <V : Serializable> Intent.putExtrasFromMap(map: Map<String, V>): Intent =
+fun Intent.putExtrasFromMap(map: Map<String, Serializable>): Intent =
     this.apply {
         map.forEach { (key: String, value: Serializable) -> putExtra(key, value) }
     }
